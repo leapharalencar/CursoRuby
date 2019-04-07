e55e3fdd1dc0138c7bf11a660ae5ad3974a556ae
leapharalencar <leapharalencar@yahoo.com.br>
2019-04-07 11:45:54 -0300

    missao3 da aula 4

--- aula4/missao3.rb
+++ aula4/missao3.rb
@@ -5,3 +5,18 @@
 # Numbers = {a: 10, b: 30 2, c: 20, d: 25, e: 15}
 
 # Crie uma instrução que seleciona o maior valor deste hash e no final imprima a chave e valor do elemento resultante.
+
+
+Numbers = {a: 10, b: 30 , c: 20, d: 25, e: 15}
+
+puts Numbers
+
+Numbers.each do |key, value|
+
+    if value.to_i > @valorMaior.to_i
+        @valorMaior = value
+        @chaveMaior = key
+    end
+end
+
+puts "O maior valor do hash é #{@valorMaior} e a sua chave é #{@chaveMaior}"
