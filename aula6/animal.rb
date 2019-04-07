3beb6a93ff256eb7abb150b34238ba183d938e3a
leapharalencar <leapharalencar@yahoo.com.br>
2019-04-07 18:22:04 -0300

    aula6

---
+++ aula6/animal.rb
@@ -0,0 +1,21 @@
+class Animal
+    def pular
+        puts 'Toing! tóim! bóim! póim!'
+    end
+
+    def dormir
+        puts 'ZzZzzzz!!!'
+    end
+end
+
+class Cachorro < Animal
+    def latir
+        puts 'Au Au'
+    end
+end
+
+
+cachorro = Cachorro.new
+cachorro.pular
+cachorro.dormir
+cachorro.latir
---
+++ aula6/computer.rb
@@ -0,0 +1,12 @@
+class Computer
+    def turn_om
+        'turn in the computer'
+    end
+
+    def shutdown
+        'shutdown the computer'
+    end
+end
+
+computer = Computer.new
+puts computer.shutdown
