8911ad997af4ea72c4fca49d84e1a8970be67a22
leapharalencar <leapharalencar@yahoo.com.br>
2019-04-07 16:12:39 -0300

    missao2 aula5

--- aula5/missao2.rb
+++ aula5/missao2.rb
@@ -1,6 +1,22 @@
 # Missão 2
-# Siga a documentação da gem cpf_cnpj para criar um programa que recebe como entrada um número de cpf e em um método verifique se este número é válido.
+# Siga a documentação da gem cpf_cnpj para criar um programa que recebe como entrada um número de 
+#cpf e em um método verifique se este número é válido.
 
 # Link da documntação:
 
 # https://github.com/fnando/cpf_cnpj
+
+require "cpf_cnpj"
+
+puts 'informar o CPF'
+number = gets.chomp
+
+cpf_cnpj = CPF.valid?(number, strict: true)
+
+if cpf_cnpj == true
+    cpf_cnpj = 'Válido'
+else
+    cpf_cnpj = 'Inválido'
+end
+
+puts "O CPF #{number} é #{cpf_cnpj}"
